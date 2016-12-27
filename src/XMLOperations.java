fb15bab7bb76910127008501a82d9c458d086750
Kowasaki <weiyinko@gmail.com>
2016-12-27 13:02:31 -0500

    Fixed uncaught parsing issues

--- src/XMLOperations.java
+++ src/XMLOperations.java
@@ -16,14 +16,16 @@ public class XMLOperations{
   public static void main(String args[]) throws Exception {
       String xmlFile = args[0];
       HashSet<String> filter = new HashSet<String>();
-      String[] listofjournals = new String[]{"<citedWork>NATURE</citedWork>",
-      "<citedWork>SCIENCE</citedWork>", "<citedWork>CELL</citedWork>",
-      "<citedWork>NEW ENGLAND JOURNAL OF MEDICINE</citedWork>",
-      "<citedWork>JAMA</citedWork>","<citedWork>LANCET",
-      "<citedWork>NATURE MEDICINE</citedWork>","<citedWork>NATURE GENERTICS</citedWork>",
-      "<citedWork>NATURE MATERIALS</citedWork>","<citedWork>NATURE IMMUNOLOGY</citedWork>",
-      "<citedWork>NATURE NANOTECHNOLOGY</citedWork>","<citedWork>NATURE BIOTECHNOLOGY</citedWork>",
-      "<citedWork>CANCER CELL</citedWork>","<citedWork>CELL STEM CELL</citedWork>","<citedWork>CA: A CANCER JOURNAL FOR CLINICIANS</citedWork>" };
+      String[] listofjournals = new String[]{"<title type=\"source\">NATURE</title>",
+      "<title type=\"source\">SCIENCE</title>", "<title type=\"source\">CELL</title>",
+      "<title type=\"source\">NEW ENGLAND JOURNAL OF MEDICINE</title>",
+      "<title type=\"source\">JAMA","<title type=\"source\">LANCET",
+      "<title type=\"source\">NATURE MEDICINE</title>","<title type=\"source\">NATURE GENERTICS</title>",
+      "<title type=\"source\">NATURE MATERIALS</title>","<title type=\"source\">NATURE IMMUNOLOGY</title>",
+      "<title type=\"source\">NATURE NANOTECHNOLOGY</title>","<title type=\"source\">NATURE BIOTECHNOLOGY</title>",
+      "<title type=\"source\">CANCER CELL</title>","<title type=\"source\">CELL STEM CELL</title>",
+      "<title type=\"source\">CA-A CANCER JOURNAL FOR CLINICIANS</title>" };
+
       filter.addAll(Arrays.asList(listofjournals));
 
 
@@ -69,7 +71,7 @@ public class XMLOperations{
             }
         }
         bw.close();
-        findCommonRef(outputName);
+        // findCommonRef(outputName);
 
       }
       catch (Exception e){
@@ -77,11 +79,11 @@ public class XMLOperations{
       }
   }
 
-  private static void findCommonRef(String output){
-    BufferedWriter bw = new BufferedWriter(new FileWriter(output));
-
-
-  }
+  // private static void findCommonRef(String output)throws Exception{
+  //   BufferedWriter bw = new BufferedWriter(new FileWriter(output));
+  //   System.out.println("TODO");
+  //   bw.close();
+  // }
 
 
   private static void parseNode(String node,BufferedWriter output) throws Exception{
@@ -99,12 +101,12 @@ public class XMLOperations{
     ArrayList<String> authorArr = new ArrayList<String>();
     ArrayList<String> yearArr = new ArrayList<String>();
     ArrayList<String> doiArr = new ArrayList<String>();
-    ArrayList<String> citedAuthorArr = new ArrayList<String>();
-    ArrayList<String> citedTitleArr = new ArrayList<String>();
-    ArrayList<String> citedWorkArr = new ArrayList<String>();
-    ArrayList<String> citedYearArr = new ArrayList<String>();
-    ArrayList<String> citedDoiArr = new ArrayList<String>();
-    ArrayList<String> refID = new ArrayList<String>();
+    // ArrayList<String> citedAuthorArr = new ArrayList<String>();
+    // ArrayList<String> citedTitleArr = new ArrayList<String>();
+    // ArrayList<String> citedWorkArr = new ArrayList<String>();
+    // ArrayList<String> citedYearArr = new ArrayList<String>();
+    // ArrayList<String> citedDoiArr = new ArrayList<String>();
+    // ArrayList<String> refID = new ArrayList<String>();
 
     NodeList recList = doc.getElementsByTagName("REC");
 
@@ -114,29 +116,50 @@ public class XMLOperations{
           Element e = (Element) nNode;
           NodeList titleList = e.getElementsByTagName("titles");
 
-            Node title = titleList.item(0);
-            if (title.getNodeType() == Node.ELEMENT_NODE) {
-              Element eElement = (Element) title;
-                int ti =  eElement.getElementsByTagName("title").getLength();
-                // String journal =  eElement.getElementsByTagName("title").item(0).getTextContent();
-                // System.out.println("Journal : " + journal);
-                journalArr.add(eElement.getElementsByTagName("title").item(0).getTextContent().replaceAll(",", " "));
-                if (ti >=6){
-                  // String article =  eElement.getElementsByTagName("title").item(5).getTextContent();
-                  // System.out.println("Article : " + article);
-                  titleArr.add(eElement.getElementsByTagName("title").item(5).getTextContent());
-                }
-            }
-
-          NodeList authorList = e.getElementsByTagName("names");
-          Node author = authorList.item(0);
-          if (author.getNodeType() == Node.ELEMENT_NODE) {
-            Element eElement = (Element) author;
-            // String frstAuth =  eElement.getElementsByTagName("display_name").item(0).getTextContent();
-            // System.out.println("Author : " + frstAuth);
-            authorArr.add("\""+eElement.getElementsByTagName("display_name").item(0).getTextContent()+"\"");
-
+          Node title = titleList.item(0);
+          if (title.getNodeType() == Node.ELEMENT_NODE) {
+            Element eElement = (Element) title;
+              int ti =  eElement.getElementsByTagName("title").getLength();
+              // String journal =  eElement.getElementsByTagName("title").item(0).getTextContent();
+              // System.out.println("Journal : " + journal);
+              journalArr.add(eElement.getElementsByTagName("title").item(0).getTextContent().replaceAll(",", " ").replaceAll("\"", " "));
+              // if(journalArr.get(0).contains("Type 1")) System.out.println(titleArr.get(0));
+              if (ti >=6){
+                // String article =  eElement.getElementsByTagName("title").item(5).getTextContent();
+                // System.out.println("Article : " + article);
+                // String test = eElement.getElementsByTagName("title").item(5).getTextContent().replaceAll(",", " ").replaceAll("\"", " ");
+                // if(test.contains("\"")||test.contains(",")) System.out.println(test);
+                titleArr.add(eElement.getElementsByTagName("title").item(5).getTextContent().replaceAll(",", " ").replaceAll("\"", " "));
+                // if(titleArr.get(0).contains("Fibrosis")) System.out.println(titleArr.get(0));
+              }
           }
+          StringBuilder pAuthor = new StringBuilder("");
+          HashSet<String> names = new HashSet<String>();
+          NodeList authorList = e.getElementsByTagName("name");
+          // System.out.println(authorList.getLength());
+          for (int i = 0; i < authorList.getLength(); i++){
+            Node author = authorList.item(i);
+            if (author.getNodeType() == Node.ELEMENT_NODE) {
+              Element eElement = (Element) author;
+              // String frstAuth =  eElement.getElementsByTagName("display_name").item(0).getTextContent();
+              // System.out.println("Author : " + frstAuth);
+              if(eElement.getAttribute("role").equals("author") && !names.contains(eElement.getElementsByTagName("display_name").item(0).getTextContent())){
+                // System.out.println(eElement.getElementsByTagName("display_name").item(0).getTextContent());
+                pAuthor.append(eElement.getElementsByTagName("display_name").item(0).getTextContent()) ;
+                pAuthor.append(",");
+                names.add(eElement.getElementsByTagName("display_name").item(0).getTextContent());
+                // if(eElement.getElementsByTagName("display_name").item(0).getTextContent().equals("Haider, AS")){
+                //   System.out.println(titleArr.get(0));
+                // }
+              }
+            }
+          }
+          // if(pAuthor.capacity() > 1){
+            // System.out.println(pAuthor.length());
+          if(pAuthor.length() > 0 && pAuthor.charAt(pAuthor.length() - 1) == ',') pAuthor.deleteCharAt(pAuthor.length() - 1);
+          // }
+          authorArr.add("\""+pAuthor.toString()+"\"");
+
 
           NodeList dateList = e.getElementsByTagName("pub_info");
           Node date = dateList.item(0);
@@ -159,71 +182,104 @@ public class XMLOperations{
               if(eElement.getAttribute("type").equals("doi")){
                 // String doi =  eElement.getAttribute("value");
                 // System.out.println("DOI : " + doi);
-                doiArr.add(eElement.getAttribute("value"));
+                doiArr.add(eElement.getAttribute("value").replaceAll(",","."));
               }
             }
           }
 
           NodeList referenceList = e.getElementsByTagName("reference");
-          System.out.println("ref: " + referenceList.getLength());
-          if(referenceList.getLength() > 0){
-              Node ref = referenceList.item(0);
-              if (ref.getNodeType() == Node.ELEMENT_NODE) {
-                Element eElement = (Element) ref;
-                int cA = eElement.getElementsByTagName("citedAuthor").getLength();
-                int cT = eElement.getElementsByTagName("citedTitle").getLength();
-                int cW = eElement.getElementsByTagName("citedWork").getLength();
-                int cY = eElement.getElementsByTagName("year").getLength();
-                int cD = eElement.getElementsByTagName("doi").getLength();
-                String Authors = "";
-                if(cA > 0){
-                  for ( int i = 0; i < cA; i ++ ){
-                    // String citedAuthor =  eElement.getElementsByTagName("citedAuthor").item(i).getTextContent();
-                    // System.out.println("Cited Author : " + citedAuthor);
-                    Authors = Authors + eElement.getElementsByTagName("citedAuthor").item(i).getTextContent();
-                    Authors = "\"" + Authors;
-                    if(i < cA) Authors = Authors + ",";
-                  }
-                  Authors = Authors + "\"";
-                  citedAuthorArr.add(Authors);
-                }
-                if(cT > 0){
-                  // String citedTitle =  eElement.getElementsByTagName("citedTitle").item(0).getTextContent();
-                  // System.out.println("Cited Title : " + citedTitle);
-                  citedTitleArr.add(eElement.getElementsByTagName("citedTitle").item(0).getTextContent());
-                }
-                if (cW > 0){
-                  String citedWork =  eElement.getElementsByTagName("citedWork").item(0).getTextContent();
-                  System.out.println("Cited Work : " + citedWork);
-                  citedWorkArr.add(eElement.getElementsByTagName("citedWork").item(0).getTextContent());
-                }
-                if(cY > 0){
-                  // String citedYear = eElement.getElementsByTagName("year").item(0).getTextContent();
-                  // System.out.println("Year : " + citedYear);
-                  citedYearArr.add(eElement.getElementsByTagName("year").item(0).getTextContent());
-                }
-                if(cD > 0){
-                  // String citedDOI = eElement.getElementsByTagName("doi").item(0).getTextContent();
-                  // System.out.println("Cited DOI : " + citedDOI);
-                  citedDoiArr.add(eElement.getElementsByTagName("doi").item(0).getTextContent());
-                }
+          // System.out.println("ref: " + referenceList.getLength());
+          // if(referenceList.getLength() > 0){
+          for(int r = 0; r < referenceList.getLength(); r++){
+            Node ref = referenceList.item(r);
+            if (ref.getNodeType() == Node.ELEMENT_NODE) {
+              Element eElement = (Element) ref;
+              int cA = eElement.getElementsByTagName("citedAuthor").getLength();
+              int cT = eElement.getElementsByTagName("citedTitle").getLength();
+              int cW = eElement.getElementsByTagName("citedWork").getLength();
+              int cY = eElement.getElementsByTagName("year").getLength();
+              int cD = eElement.getElementsByTagName("doi").getLength();
+              StringBuilder Authors = new StringBuilder("");
+
+              if(cT > 0){
+                // String citedTitle =  eElement.getElementsByTagName("citedTitle").item(0).getTextContent();
+                // System.out.println("Cited Title : " + citedTitle);
+                  // citedTitleArr.add(eElement.getElementsByTagName("citedTitle").item(0).getTextContent());
+                  // output.write(citedTitleArr.get(0)+",");
+                  output.write(eElement.getElementsByTagName("citedTitle").item(0).getTextContent().replaceAll("," ," ")+",");
+
+              }else{
+                output.write(" ,");
               }
+              if(cA > 0){
+                for ( int i = 0; i < cA; i ++ ){
+                  // String citedAuthor =  eElement.getElementsByTagName("citedAuthor").item(i).getTextContent();
+                  // System.out.println("Cited Author : " + citedAuthor);
+                  Authors.append("\"");
+                  Authors.append(eElement.getElementsByTagName("citedAuthor").item(i).getTextContent());
+                  // Authors = "\"" + Authors;
+                  if(i < cA-1) Authors.append(",");
+                }
+                Authors.append("\"");
+                // citedAuthorArr.add(Authors);
+                output.write(Authors.append(",").toString());
+              }else{
+                output.write(" ,");
+              }
+              if(cY > 0){
+                // String citedYear = eElement.getElementsByTagName("year").item(0).getTextContent();
+                // System.out.println("Year : " + citedYear);
+                // citedYearArr.add(eElement.getElementsByTagName("year").item(0).getTextContent());
+                output.write(eElement.getElementsByTagName("year").item(0).getTextContent()+",");
+              }else{
+                output.write(" ,");
+              }
+
+              if(cD > 0){
+                // String citedDOI = eElement.getElementsByTagName("doi").item(0).getTextContent();
+                // System.out.println("Cited DOI : " + citedDOI);
+                // citedDoiArr.add(eElement.getElementsByTagName("doi").item(0).getTextContent());
+                // if(eElement.getElementsByTagName("doi").item(0).getTextContent().contains(",")){
+                //   System.out.println(eElement.getElementsByTagName("doi").item(0).getTextContent());
+                //   System.out.println(eElement.getElementsByTagName("doi").item(0).getTextContent().replaceAll(",","."));
+                // }
+                output.write(eElement.getElementsByTagName("doi").item(0).getTextContent().replaceAll(",",".")+",");
+              }else{
+                output.write(" ,");
+              }
+              if (cW > 0){
+                // String citedWork =  eElement.getElementsByTagName("citedWork").item(0).getTextContent();
+                // System.out.println("Cited Work : " + citedWork);
+                // citedWorkArr.add(eElement.getElementsByTagName("citedWork").item(0).getTextContent());
+                output.write("\"" + eElement.getElementsByTagName("citedWork").item(0).getTextContent()+ "\"" +",");
+              }else{
+                output.write(" ,");
+              }
+
+              if(titleArr.size() > 0){output.write(titleArr.get(0) + ",");} else {output.write(" ,");}
+              if(authorArr.size() > 0){output.write(authorArr.get(0) + ",");} else {output.write(" ,");}
+              if(yearArr.size() > 0){output.write(yearArr.get(0) + ",");} else {output.write(" ,");}
+              if(doiArr.size() > 0){output.write(doiArr.get(0) + ",");} else {output.write(" ,");}
+              if(journalArr.size() > 0){output.write(journalArr.get(0) + ",");} else {output.write(" ,");}
+              output.write(" \n");
+            }
           }
+          // }
         }
       }
-      for(int i = 0; i < 1; i++){
-        if(titleArr.size() > i){output.write(titleArr.get(i)+",");} else {output.write(",");}
-        if (authorArr.size() > i){output.write(authorArr.get(i)+",");} else {output.write(",");}
-        if (yearArr.size() > i){output.write(yearArr.get(i)+",");} else {output.write(",");}
-        if (doiArr.size() > i){output.write(doiArr.get(i)+",");} else {output.write(",");}
-        if (journalArr.size() > i){output.write(journalArr.get(i)+",");} else {output.write(",");}
-        if (citedTitleArr.size() > i){output.write(citedTitleArr.get(i)+","); } else {output.write(",");}
-        if (citedAuthorArr.size() > i){output.write(citedAuthorArr.get(i)+","); } else {output.write(",");}
-        if (citedYearArr.size() > i){ output.write(citedYearArr.get(i)+",");} else {output.write(",");}
-        if (citedDoiArr.size() > i) {output.write(citedDoiArr.get(i)+",");} else {output.write(",");}
-        if (citedWorkArr.size() > i) {output.write(citedWorkArr.get(i)+",");} else {output.write(",");}
-        if (refID.size() > i) {output.write(refID.get(i)+"\n");} else {output.write("\n");};
-      }
+      // for(int i = 0; i < 1; i++){
+        // if(titleArr.size() > i){output.write(titleArr.get(i)+",");} else {output.write(",");}
+        // if (authorArr.size() > i){output.write(authorArr.get(i)+",");} else {output.write(",");}
+        // if (yearArr.size() > i){output.write(yearArr.get(i)+",");} else {output.write(",");}
+        // if (doiArr.size() > i){output.write(doiArr.get(i)+",");} else {output.write(",");}
+        // if (journalArr.size() > i){output.write(journalArr.get(i)+",");} else {output.write(",");}
+        // if (citedTitleArr.size() > i){output.write(citedTitleArr.get(i)+","); } else {output.write(",");}
+        // if (citedAuthorArr.size() > i){output.write(citedAuthorArr.get(i)+","); } else {output.write(",");}
+        // if (citedYearArr.size() > i){ output.write(citedYearArr.get(i)+",");} else {output.write(",");}
+        // if (citedDoiArr.size() > i) {output.write(citedDoiArr.get(i)+",");} else {output.write(",");}
+        // if (citedWorkArr.size() > i) {output.write(citedWorkArr.get(i)+",");} else {output.write(",");}
+        // if (refID.size() > i) {output.write(refID.get(i)+"\n");} else {output.write("\n");};
+      // }
 
     }
 
